29d7d441ff379d09873ea68e04cab21480860dba
Adam Dalton <adam.dalton@gmail.com>
2013-01-17 16:30:19 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VerbnetAsAService::Application.config.secret_token = '8deed6fa690e3a3d9727d9e838fade06240d0f88ff258d7e2cee583c89b48941da9daa8c49c5771100d33eddfd0655386d607f584befb114830aa449a4e45253'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+VerbnetAsAService::Application.routes.draw do
+  root :to => "home#index"
+end
